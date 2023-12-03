18cde83c3d529daa925b7dc7eb2307ebe6b55f47
Ryan <ryan@changedapps.com>
2023-12-03 15:57:50 -0600

    Working with testInput

--- problem3/problem3.js
+++ problem3/problem3.js
@@ -1,12 +1,12 @@
 
 const fs = require('fs');
 const readline = require('readline');
-var test;
+
 async function getInput (){
     try {
         var array = [];
         const rl = readline.createInterface({
-            input: fs.createReadStream('problem3/input.txt'),
+            input: fs.createReadStream('problem3/testInput.txt'),
             crlfDelay: Infinity
         })
 
@@ -14,7 +14,6 @@ async function getInput (){
             array.push(line);
         })
         await new Promise((res) => rl.once('close', res))
-        console.log(array);
         findParts(array)
     } catch (error) {
         console.log(error);
@@ -29,21 +28,56 @@ function findParts(array) {
     // console.log(array);
     for(var rowIndex in array) {
         var currentRow = array[rowIndex];
+        var previousRow = array[parseInt(rowIndex) - 1 || []]
+        var nextRow = array[parseInt(rowIndex) + 1 || []]
         // console.log("rowIndex", currentRow);
         for(var char in currentRow) {
-            if(Number(currentRow[char])) {
+            if((Number(currentRow[char]) || currentRow[char] == 0) && (currentRow[char] != ".")) {
                 if(firstIndex) {
-                    lastIndex = currentRow[char];
+                    console.log(firstIndex)
+                    lastIndex = parseInt(char) + 1;
                 } else {
-                    firstIndex = currentRow[char];
-                    lastIndex = currentRow[char];
+                    firstIndex = char;
+                    lastIndex = parseInt(char) + 1;
                 }
             } else if (firstIndex) {
-                var num = currentRow.substring(firstIndex, lastIndex);
-                console.log(num);
+                var num = parseInt(currentRow.substring(firstIndex, lastIndex));
+                if(!Number(num)) {
+                    console.log(num, firstIndex, lastIndex, rowIndex)
+                    process.exit(0)
+                }
+                if(!Number(currentRow[lastIndex]) && currentRow[lastIndex] != "." && currentRow[lastIndex]){
+                    total += num;
+                }
+                if (previousRow && previousRow.length > 0){
+                    if(checkRow(previousRow, firstIndex, lastIndex)) {
+                        total += num;
+                    };
+                }
+                if (nextRow && nextRow.length > 0) {
+                    if(checkRow(nextRow, firstIndex, lastIndex)) {
+                        total += num;
+                    };
+                }
+                firstIndex = "";
+                lastIndex = "";
             }
         }
     }
+    console.log(total)
 }
+
+function checkRow (row, first, last) {
+    var substring = row.substring(first - 1 || 0, last + 1);
+    if(!Number(substring)) {
+        for(var i = 0; i < substring.length; i++) {
+            if(!Number(substring.charAt(i)) && substring.charAt(i) != ".") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 getInput();
 findParts();
